8592ba1257e18ebaecc8e3e7fd4a569d81abdd45
Fabio Arao <fabioarao@gmail.com>
2023-08-27 12:18:35 +0100

    first commit

---
+++ interviews/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+from django.conf import settings
+
+
+class Chat(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    title = models.CharField(max_length=100, editable=False)
+    job = models.ForeignKey("jobs.Job", on_delete=models.CASCADE, related_name="chats")
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+def save (self, *args, **kwargs):
+    if not self.uuid:
+        self.uuid = uuid.uuid4()
+        self.title = f"Chat {self.job.title} - {self.uuid}"
+        super().save(*args, **kwargs)
+        initial_prompt = settings.INITIAL_PROMPT_TEMPLATE
+        initial_prompt = initial_prompt.replace("{job_title}", self.job.title)
+        initial_prompt = initial_prompt.replace("{job_requirements}", self.job.requirements)
+        initial_prompt = initial_prompt.replace("{job_responsibilities}", self.job.responsibilities)
+        Message.objects.create(
+            chat=self,
+            role="system",
+            content=initial_prompt
+        )
+    super().save(*args, **kwargs)
+
+
+class Message(models.Model):
+    ROLE_CHOICES = (
+        ("system", "System"),
+        ("user", "Candidate"),
+        ("assistant", "Interviewer")
+    )
+
+    chat = models.ForeignKey("interviews.Chat", on_delete=models.CASCADE, related_name="messages")
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.role} - {self.chat.title}"
